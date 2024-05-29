2a2a9849fc8e6fb6614193209bd682fded2cf6e0
Anhnguyen2708 <phamanhnguyen2708@gmail.com>
2024-05-29 20:03:14 +0700

    update lien he css

--- resources/views/pages/LienHe/lien_he_layout.blade.php
+++ resources/views/pages/LienHe/lien_he_layout.blade.php
@@ -20,43 +20,49 @@
             <div class="title_2LH">
                 <p class="title_2LH_content">Liên hệ</p>
             </div>
-            <p class="Forminstruction">Hãy liên hệ khi bạn gặp bất cứ vấn đề nào liên quan đến cửa hàng hoặc sản phẩm của chúng tôi. Chúng tôi sẽ cố gắng phản hồi trong thời gian sớm nhất.</p>
             <div class="LHmain">
                 <div class="formside">
-                    <div class = "form_row">
-                        <label for="name">Tên *</label>
-                        <input type="text" id="name" name="name" required>
-                    </div>
-                    <div class = "form_row">
-                        <label for="sdt">Số điện thoại</label>
-                        <input type="tel" id="sdt" name="sdt">
-                    </div>   
-                    <div class = "form_row">
-                        <label for="email">Email *</label>
-                        <input type="email" id="email" name="email" required>
-                    </div>
-                    <div class = "form_row">
-                        <label for="title">Tiêu đề *</label>
-                        <input type="text" id="title" name="title" required>
-                    </div>
-                    <div class = "form_row">
-                        <label for="question">Câu hỏi *</label>
-                        <textarea id="question" rows="3" placeholder="" name="question" required></textarea><br>
-                    </div>
-                    <div class="Submit_form">
-                        <input class="Submit_button" type="submit" value="Gửi" name="Submit">
+                    <div class = "sbc">
+                        <div class = "form_row">
+                            <p class="Forminstruction">Hãy liên hệ khi bạn gặp bất cứ vấn đề nào liên quan đến cửa hàng hoặc sản phẩm của chúng tôi. Chúng tôi sẽ cố gắng phản hồi trong thời gian sớm nhất.</p>
+                        </div>
+                        <div class = "form_row">
+                            <label for="name">Tên *</label>
+                            <input type="text" id="name" name="name" required>
+                        </div>
+                        <div class = "form_row">
+                            <label for="sdt">Số điện thoại</label>
+                            <input type="tel" id="sdt" name="sdt">
+                        </div>   
+                        <div class = "form_row">
+                            <label for="email">Email *</label>
+                            <input type="email" id="email" name="email" required>
+                        </div>
+                        <div class = "form_row">
+                            <label for="title">Tiêu đề *</label>
+                            <input type="text" id="title" name="title" required>
+                        </div>
+                        <div class = "form_row">
+                            <label for="question">Câu hỏi *</label>
+                            <textarea id="question" rows="7" placeholder="" name="question" required></textarea><br>
+                        </div>
+                        <div class="Submit_form">
+                            <input class="Submit_button" type="submit" value="Gửi" name="Submit">
+                        </div>
                     </div>
                 </div>
                 <div class="Storei4">
-                    <p style="font-weight: bold;">Thông tin cửa hàng</p>
-                    <p><img src="{{ asset('public/frontend/images/email.png') }}">Blinkiy.is334@gmail.com</p>
-                    <p><img src="{{ asset('public/frontend/images/location-pointer.png') }}">Trường Đại học Công nghệ Thông tin - ĐHQG TPHCM</p>
-                    <p><img src="{{ asset('public/frontend/images/phone-call.png') }}">0814576804</p>
-                    <p><img style="padding-top: -2px;" src="{{ asset('public/frontend/images/email.png') }}">Blinkiy.is334@gmail.com</p>
-                    <div align="center" style="width: 80%;">
-                        <button id="myButton" onclick="redirectToUrl()" type="button"><img src="{{ asset('public/frontend/images/facebook.png')}}"></button>
-                        <button id="myButton" onclick="redirectToUrl()" type="button"><img src="{{ asset('public/frontend/images/instagram.png')}}"></button>
-                        <button id="myButton " onclick="redirectToUrl()" type="button"><img src="{{ asset('public/frontend/images/tik-tok.png')}}"></button>
+                    <div class = "i4_cont">
+                        <p style="font-weight: bold;">Thông tin cửa hàng</p>
+                        <p><img src="{{ asset('public/frontend/images/email.png') }}">Blinkiy.is334@gmail.com</p>
+                        <p><img src="{{ asset('public/frontend/images/location-pointer.png') }}">Trường Đại học Công nghệ Thông tin - ĐHQG TPHCM</p>
+                        <p><img src="{{ asset('public/frontend/images/phone-call.png') }}">0814576804</p>
+                        <p><img style="padding-top: -2px;" src="{{ asset('public/frontend/images/email.png') }}">Blinkiy.is334@gmail.com</p>
+                        <div align="center">
+                            <button id="myButton" onclick="redirectToUrl()" type="button"><img src="{{ asset('public/frontend/images/facebook.png')}}"></button>
+                            <button id="myButton" onclick="redirectToUrl()" type="button"><img src="{{ asset('public/frontend/images/instagram.png')}}"></button>
+                            <button id="myButton " onclick="redirectToUrl()" type="button"><img src="{{ asset('public/frontend/images/tik-tok.png')}}"></button>
+                        </div>
                     </div>
                 </div>
             </div>
