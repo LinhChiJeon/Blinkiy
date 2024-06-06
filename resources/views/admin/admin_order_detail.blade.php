7e80768fbc23a50b83b4acc689c2fd1a4528d4ad
Anhnguyen2708 <phamanhnguyen2708@gmail.com>
2024-06-07 02:42:27 +0700

    update

--- resources/views/admin/admin_order_detail.blade.php
+++ resources/views/admin/admin_order_detail.blade.php
@@ -5,20 +5,20 @@
     <h1>Hóa Đơn</h1>
     @if ($order)
         <div class="invoice_section" style="background: white;">
-            <h2>Thông tin khách hàng</h2>
+            <h2 style="font-weight:bold">Thông tin khách hàng</h2>
             <p>Họ tên: {{ $order->customer_name ?? '' }}</p>
             <p>Email: {{ $order->customer_email ?? '' }}</p>
             <p>SĐT: {{ $order->customer_phone ?? '' }}</p>
             <p>Địa chỉ: {{ $order->customer_address ?? '' }}</p>
         </div>
         <div class="invoice_section">
-            <h2>Thông tin đơn hàng</h2>
+            <h2 style="font-weight:bold">Thông tin đơn hàng</h2>
             <p>Mã đơn hàng: {{ $order->order_id ?? '' }}</p>
             <p>Ngày đặt hàng: {{ $order->order_date ?? '' }}</p>
             <p>Mô tả: {{ $order->order_note ?? '' }}</p>
         </div>
         <div class="invoice_section">
-            <h2>Phương thức thanh toán</h2>
+            <h2 style="font-weight:bold">Phương thức thanh toán</h2>
             @switch($order->payment_opt)
                 @case(1)
                     <p>Thanh toán bằng MoMo QR Code</p>
@@ -32,22 +32,23 @@
             @endswitch
         </div>
         <div class="invoice_section">
-            <h2>Chi tiết sản phẩm</h2>
+            <h2 style="font-weight:bold">Chi tiết sản phẩm</h2>
             @foreach ($orderDetails as $item)
                 <div class="item_admin">
-                    <p class="item_name">{{ $item->product->product_name }}</p>
-                    <p class="item_color">Màu: {{ $item->product->product_color }}</p>
-                    <p class="item_size">Kích cỡ: {{ $item->size_id }}</p>
-                    <p class="item_quantity">SL: {{ $item->product_quantity }}</p>
-                    <p class="item_price">Đơn giá: {{ number_format($item->product_price, 0, ',', '.') }} ₫</p>
-                    <p class="item_total">Thành tiền: {{ number_format($item->total_price, 0, ',', '.') }} ₫</p>
+                    <p class="item_name_admin">{{ $item->product->product_name }}</p>
+                    <p class="item_color_admin">Màu: {{ $item->product->product_color }}</p>
+                    <p class="item_size_admin">Kích cỡ: {{ $item->size_id }}</p>
+                    <p class="item_quantity_admin">SL: {{ $item->product_quantity }}</p>
+                        <p class="item_price_admin">Đơn giá: {{ number_format($item->product_price, 0, ',', '.') }} ₫</p>
+                        <p class="item_total_admin" style="font-weight:bold">Thành tiền: {{ number_format($item->total_price, 0, ',', '.') }} ₫</p>
+                    <hr>
                 </div>
             @endforeach
         </div>
         <div class="invoice_section">
-            <p>Tổng tiền: {{ number_format($order->order_total_price, 0, ',', '.') }} ₫</p>
-            <p>Phí giao hàng: 30,000 ₫</p>
-            <p>Thành tiền: {{ number_format($order->order_total_price + 30000, 0, ',', '.') }} ₫</p>
+            <p style="font-weight:bold">Tổng tiền: {{ number_format($order->order_total_price, 0, ',', '.') }} ₫</p>
+            <p style="font-weight:bold">Phí giao hàng: 30,000 ₫</p>
+            <p style="font-weight:bold">Thành tiền: {{ number_format($order->order_total_price + 30000, 0, ',', '.') }} ₫</p>
         </div>
         <div class="invoice_section">
             <form action="{{ route('accept_order', ['id' => $order->order_id]) }}" method="POST" style="display:inline;">
@@ -62,7 +63,8 @@
             <form action="{{ route('done_order', ['id' => $order->order_id]) }}" method="POST" style="display:inline;">
                 @csrf
                 @method('DELETE')
-                <button type="submit" class="btn btn-danger">Hoàn thành đơn hàng</button>
+                <button type="submit" class="btn btn-danger"
+                style="background-color:dodgerblue; border-color:dodgerblue">Hoàn thành đơn hàng</button>
             </form>
         </div>
     @else
